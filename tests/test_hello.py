24da908ddc8bdec59a4d52a890f3ea2f1288c740
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2025-10-06 13:58:32 +0200

    add test

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,3 +4,4 @@ from media.hello import say_hello
 def test_say_hello():
     assert say_hello("World") == "Hello, World!"
     assert say_hello("Alice") == "Hello, Alice!"
+    assert say_hello("Bob") == "Hello, Bob!"
